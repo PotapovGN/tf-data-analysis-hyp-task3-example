1156c7d9d556f2aeca0f9a69a3b8c514de1ffe34
Gleb Potapov <122749679+PotapovGN@users.noreply.github.com>
2023-04-13 22:04:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,6 @@ from scipy.stats import ttest_ind
 chat_id = 485082255 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool: 
-    t_stat, p_val = ttest_ind(control, test)
+    t_stat, p_val = ttest_ind(x, y)
     
     return p_val < 0.09
